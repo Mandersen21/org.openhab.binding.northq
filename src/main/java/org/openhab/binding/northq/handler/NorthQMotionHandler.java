7116d833c5a107345db83ffc3523dc6d0e741281
Philip1992 <philler10@hotmail.com>
2017-10-11 10:30:43 +0200

    qMotion arm/disarm

--- src/main/java/org/openhab/binding/northq/handler/NorthQMotionHandler.java
+++ src/main/java/org/openhab/binding/northq/handler/NorthQMotionHandler.java
@@ -10,14 +10,21 @@ package org.openhab.binding.northq.handler;
 
 import static org.openhab.binding.northq.NorthQBindingConstants.CHANNEL_QMOTION;
 
+import java.util.ArrayList;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
+import org.openhab.binding.northq.internal.common.NorthQConfig;
+import org.openhab.binding.northq.internal.helpers.TokenHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.Qmotion;
+import services.NorthqServices;
 
 /**
  * The {@link NorthQMotionHandler} is responsible for handling commands, which are
@@ -28,20 +35,70 @@ import org.slf4j.LoggerFactory;
 @NonNullByDefault
 public class NorthQMotionHandler extends BaseThingHandler {
 
-    // public static final ThingTypeUID SUPPORTED_THING_TYPE = THING_TYPE_QMOTION;
+    private TokenHelper tokenHelper;
+    private NorthqServices services;
+    private NorthQConfig config;
 
     @SuppressWarnings("null")
     private final Logger logger = LoggerFactory.getLogger(NorthQMotionHandler.class);
 
     public NorthQMotionHandler(Thing thing) {
         super(thing);
+        tokenHelper = new TokenHelper();
+        services = new NorthqServices();
+        config = new NorthQConfig();
     }
 
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
         if (channelUID.getId().equals(CHANNEL_QMOTION)) {
-            System.out.print("Registered interactions with motion");
+
+            // Configurations
+            String gateway_id = NorthQConfig.getGATEWAY_ID();
+            String username = NorthQConfig.getUSERNAME();
+            String password = NorthQConfig.getPASSWORD();
+            Qmotion qMotion = null;
+
+            ArrayList<model.Thing> things = NorthQConfig.NETWORK.getGateways().get(0).getThings();
+            for (int i = 0; i < things.size(); i++) {
+                if (things.get(i) instanceof model.Qmotion) {
+                    qMotion = (Qmotion) things.get(i);
+                }
+            }
+
+            System.out.println("Token in config " + NorthQConfig.getTOKEN());
+            System.out.println("Dan token: " + NorthQConfig.NETWORK.getToken());
+
+            // Get token from helper
+            // tokenHelper.getToken(username, password);
+            // String token = NorthQConfig.getTOKEN();
+
+            if (command.toString().equals("ON")) {
+                // Plug should be turned on
+                try {
+                    services.armMotion(NorthQConfig.NETWORK.getUserId(), NorthQConfig.NETWORK.getToken(),
+                            NorthQConfig.GATEWAY_ID, qMotion);
+                } catch (Exception e) {
+                    // TODO: Add more exceptions
+                    updateStatus(ThingStatus.OFFLINE);
+                }
+            } else {
+                // Plug should be turned off
+                try {
+                    services.disarmMotion(NorthQConfig.NETWORK.getUserId(), NorthQConfig.NETWORK.getToken(),
+                            NorthQConfig.GATEWAY_ID, qMotion);
+                } catch (Exception e) {
+                    // TODO: Add more exceptions
+                    updateStatus(ThingStatus.OFFLINE);
+                }
+            }
         }
+
+        // if (channelUID.getId().equals(CHANNEL_QMOTION))
+        //
+        // {
+        // System.out.print("Registered interactions with motion");
+        // }
     }
 
     @Override
