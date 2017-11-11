40f0b75d05332c03f7c2c706856752ae99dcf60e
Jakob Skriver <Jakob_skiver@hotmail.com>
2017-11-11 11:11:55 +0100

    SettingDone

--- src/main/java/org/openhab/binding/northq/handler/SettingsHandler.java
+++ src/main/java/org/openhab/binding/northq/handler/SettingsHandler.java
@@ -2,18 +2,15 @@ package org.openhab.binding.northq.handler;
 
 import static org.openhab.binding.northq.NorthQBindingConstants.*;
 
-import java.util.ArrayList;
-
-import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
+import org.openhab.binding.northq.NorthQBindingConstants;
 import org.openhab.binding.northq.internal.common.NorthQConfig;
 import org.openhab.binding.northq.internal.common.ReadWriteLock;
-import org.openhab.binding.northq.internal.model.NGateway;
-import org.openhab.binding.northq.internal.model.Qsettings;
-import org.openhab.binding.northq.internal.model.Thing;
 
 public class SettingsHandler extends BaseThingHandler {
 
@@ -25,32 +22,30 @@ public class SettingsHandler extends BaseThingHandler {
     public void handleCommand(ChannelUID channelUID, Command command) {
         try {
             ReadWriteLock.getInstance().lockRead();
-            Qsettings qSet = getQsettings();
-            String temperature = command.toString();
 
-            if (qSet == null) {
-                updateStatus(ThingStatus.OFFLINE);
-                return;
-            } else {
-                updateStatus(ThingStatus.ONLINE);
-            }
+            updateState(NorthQBindingConstants.CHANNEL_SETTINGS_TOGGLEHEATLOCATION,
+                    NorthQConfig.isHEATONLOCATION() ? OnOffType.ON : OnOffType.OFF);
+            updateState(NorthQBindingConstants.CHANNEL_SETTINGS_ISHOMETEMP,
+                    DecimalType.valueOf(String.valueOf(NorthQConfig.getISHOMETEMP())));
+            updateState(NorthQBindingConstants.CHANNEL_SETTINGS_NOTHOMETEMP,
+                    DecimalType.valueOf(String.valueOf(NorthQConfig.getNOTHOMETEMP())));
 
             if (channelUID.getId().equals(CHANNEL_SETTINGS_TOGGLEHEATLOCATION)) {
                 if (command.toString().equals("ON")) {
-                    qSet.getQsettings().toggleHeatOnLocation = true;
+                    NorthQConfig.setHEATONLOCATION(true);
 
                 } else if (command.toString().equals("OFF")) {
-                    qSet.getQsettings().toggleHeatOnLocation = false;
+                    NorthQConfig.setHEATONLOCATION(false);
                 }
             }
             if (channelUID.getId().equals(CHANNEL_SETTINGS_ISHOMETEMP)) {
                 if (command.toString() != null) {
-                    qSet.getQsettings().isHomeTemp = Float.valueOf(temperature);
+                    NorthQConfig.setISHOMETEMP(Float.valueOf(command.toString()));
                 }
             }
             if (channelUID.getId().equals(CHANNEL_SETTINGS_NOTHOMETEMP)) {
                 if (command.toString() != null) {
-                    qSet.getQsettings().notHomeTemp = Float.valueOf(temperature);
+                    NorthQConfig.setNOTHOMETEMP(Float.valueOf(command.toString()));
                 }
             }
         } catch (Exception e) {
@@ -78,20 +73,6 @@ public class SettingsHandler extends BaseThingHandler {
     @Override
     public void initialize() {
         updateStatus(ThingStatus.ONLINE);
-    }
-
-    public @Nullable Qsettings getQsettings() {
-        ArrayList<NGateway> gateways = NorthQConfig.NETWORK.getGateways();
-        for (NGateway gw : gateways) {
-            ArrayList<Thing> things = gw.getThings();
-            for (int i = 0; i < things.size(); i++) {
-                if (things.get(i) instanceof Qsettings) {
-                    Qsettings qSettings = (Qsettings) things.get(i);
-                    return qSettings;
-                }
-            }
-        }
-        return null;
     }
 
 }
